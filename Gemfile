387e06e29ac453820cd7d4c054c0a7576f2bfc08
Shahad yousif <shahad_yousif38@yahoo.com>
2017-05-11 16:27:23 +0000

    add bootstrap and geocoder gem

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'https://rubygems.org'
 
+
 gem 'devise'
+gem 'bootstrap-sass', '3.2.0.2'
+gem 'geocoder'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
 # Use sqlite3 as the database for Active Record
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511143657) do
+ActiveRecord::Schema.define(version: 20170511155212) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,6 +30,10 @@ ActiveRecord::Schema.define(version: 20170511143657) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
+    t.date     "date_of_birth"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
